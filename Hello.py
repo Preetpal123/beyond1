153755af392607bee32f8e0e8c20405f279a7b51
Preetpal Singh <Hinton0024@gmail.com>
2021-03-25 13:04:17 +0000

    Fresh Start

---
+++ Hello.py
@@ -0,0 +1,10 @@
+'''
+from cs50 import get_string
+name = get_string("What is your Name?")
+print(f"My name is {name}")
+#format strings in python
+'''
+#from PIL import image, ImageFilter
+#before = Image.open("bridge.bmp")
+print("Hello World")
+
